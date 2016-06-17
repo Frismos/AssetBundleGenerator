b79bf888696f84c1b052f8a816289cecdbaca4e6
Murad Chnchghukyan <muradramas@gmail.com>
2016-06-17 10:59:38 +0400

    Initial Import

---
+++ Assets/Scripts/CreateAseetBundleMenuItem.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+public class CreateAssetBundles{
+
+	[MenuItem ("Assets/Build Asset Bundles/Build AssetBundles for Editor")]
+	
+	static void BuildAllAssetBundles (){
+		BuildPipeline.BuildAssetBundles("Assets/Asset Bundles/Editor");
+	}
+
+	[MenuItem ("Assets/Build Asset Bundles/Build AssetBundles for Android")]
+
+	static void BuildAndroidAssetBundles (){
+		BuildPipeline.BuildAssetBundles("Assets/Asset Bundles/Android", BuildAssetBundleOptions.None, BuildTarget.Android);
+	}
+
+	[MenuItem ("Assets/Build Asset Bundles/Build AssetBundles for iOS")]
+	
+	static void BuildIOSAssetBundles (){
+		BuildPipeline.BuildAssetBundles("Assets/Asset Bundles/iOS", BuildAssetBundleOptions.None, BuildTarget.iOS);
+	}
+}
+#endif
